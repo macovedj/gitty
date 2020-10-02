d3201daa08fdafb8585841030841a2293aa5e22c
DM34461 <daniel.macovei@kroger.com>
2020-10-01 21:51:46 -0400

    add branch select form

--- lib/gitty_web/controllers/repo_controller.ex
+++ lib/gitty_web/controllers/repo_controller.ex
@@ -8,10 +8,25 @@ defmodule GittyWeb.RepoController do
     repo = %RootCommit{sha: sha}
     contents = Repos.show_root(repo)
     Repos.get_most_recent_commit()
-    render(conn, "index.html", contents: contents, name: "gitty")
+    render(conn, "index.html", contents: contents, name: "gitty", branches: Repos.get_branches())
   end
 
   def show(conn, %{"id" => id}) do
+    # sha = Repos.get_most_recent_commit(id)
+    # repo = %RootCommit{sha: sha}
+    # {content, 0} = {Repos.ls_folder(sha, %{"object_type" => "commit"}), 0}
+    # contents = Repos.show_root(%RootCommit{sha: sha})
+    # branches = Repos.get_branches()
+    # {content, 0} = case type do
+    #   "blob" ->
+    #     Repos.cat_file(hash)
+    #   "tree" ->
+    #     { Repos.ls_folder(hash),0}
+    #   _ ->
+    #     {"default", 0}
+    # end
+    # IO.inspect(content)
+    # render(conn, "show.html", contents: content)
     render(conn, "show.html", type: id)
   end
 end
